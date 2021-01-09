3e9c5313cfeafeed783b509f347165a8a08a817a
guoxbin <guoxbin@gmail.com>
2021-01-09 23:02:42 +0800

    Attach command

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# wingchain-cli
+wingchain-cli
+=============
+
+
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/wingchain-cli.svg)](https://npmjs.org/package/wingchain-cli)
+[![Downloads/week](https://img.shields.io/npm/dw/wingchain-cli.svg)](https://npmjs.org/package/wingchain-cli)
+[![License](https://img.shields.io/npm/l/wingchain-cli.svg)](https://github.com/guoxbin/wingchain-cli/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g wingchain-cli
+$ wingchain-cli COMMAND
+running command...
+$ wingchain-cli (-v|--version|version)
+wingchain-cli/0.1.0 darwin-x64 node-v11.15.0
+$ wingchain-cli --help [COMMAND]
+USAGE
+  $ wingchain-cli COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+* [`wingchain-cli attach [FILE]`](#wingchain-cli-attach-file)
+
+## `wingchain-cli attach [FILE]`
+
 Wingchain CLI
+
+```
+USAGE
+  $ wingchain-cli attach [FILE]
+
+OPTIONS
+  -f, --force
+  -h, --help       show CLI help
+  -n, --name=name  name to print
+  -v, --version    show CLI version
+```
+
+_See code: [src/commands/attach.ts](https://github.com/wingchain/wingchain-cli/blob/v0.1.0/src/commands/attach.ts)_
+<!-- commandsstop -->
---
+++ src/commands/attach.ts
@@ -0,0 +1,58 @@
+import {Command, flags} from '@oclif/command'
+
+class Attach extends Command {
+  static description = 'Attach to a node'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    // flag with a value (-n, --name=VALUE)
+    endpoint: flags.string({char: 'e', description: 'Node endpoint to attach'}),
+  }
+
+  static args = []
+
+  async run() {
+    const {args, flags} = this.parse(Attach)
+
+    const endpoint = flags.endpoint || 'http://localhost:3109'
+
+    const repl = require('repl')
+    const vm = require('vm')
+    const {Sdk, JsonRpcClient} = require('wingchain-sdk')
+
+    const {processTopLevelAwait} = require('node-repl-await')
+
+    function isRecoverableError(error: any) {
+      if (error.name === 'SyntaxError') {
+        return /^(Unexpected end of input|Unexpected token)/.test(
+          error.message,
+        )
+      }
+      return false
+    }
+
+    async function customEval(code: any, context: any, filename: any, callback: any) {
+      code = processTopLevelAwait(code) || code
+
+      try {
+        const result = await vm.runInNewContext(code, context)
+        callback(null, result)
+      } catch (error) {
+        if (isRecoverableError(error)) {
+          callback(new repl.Recoverable(error))
+        } else {
+          console.log(error)
+        }
+      }
+    }
+
+    const client = new JsonRpcClient(endpoint)
+    const sdk = new Sdk(client)
+
+    const c = repl.start({prompt: 'wingchain-cli> ', eval: customEval}).context
+    c.util.inspect.defaultOptions.depth = null
+    c.sdk = sdk
+  }
+}
+
+export = Attach
